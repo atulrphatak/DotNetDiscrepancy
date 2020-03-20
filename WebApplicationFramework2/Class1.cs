baf5df32ddffa6fb7c09cf9e827e820516d0dad2
atul phatak <atul.phatak5@outlook.com>
2020-03-20 16:38:19 +0530

    Add project files.

---
+++ WebApplicationFramework2/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mail;
+using System.Net.Mail;
+using System.Runtime.CompilerServices;
+using System.Globalization;
+
+[assembly: StringFreezingAttribute()]
+
+
+
+namespace WebApplicationFramework2
+{
+
+    public class Class1
+    {
+        public static void Main()
+        {
+            CultureInfo current = CultureInfo.CurrentCulture;
+            Console.WriteLine("The current culture is {0}", current.Name);
+            CultureInfo newCulture;
+            if (current.Name.Equals("fr-FR"))
+                newCulture = new CultureInfo("fr-LU");
+            else
+                newCulture = new CultureInfo("fr-sFR");
+
+            //CultureInfo.CurrentCulture = newCulture;
+            Console.WriteLine("The current culture is now {0}",
+                              CultureInfo.CurrentCulture.Name);
+        }
+    }
+}
